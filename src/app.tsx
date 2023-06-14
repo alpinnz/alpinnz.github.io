6f78ae48359fe5a2cc9b4e308af22eb52f0375fa
Alfin Noviaji <alpinnz@gmail.com>
2023-06-14 20:37:25 +0700

    tailwindcss

--- src/app.tsx
+++ src/app.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 
 export default function App(): React.ReactElement {
     return (
-        <div className="app">
-        </div>
+        <h1 className="text-3xl font-bold underline">
+            Hello world!
+        </h1>
     );
 }
