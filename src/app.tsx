c278d16e6584722fe746d5a53bec206c33743e5b
Alfin Noviaji <alpinnz@gmail.com>
2023-06-16 17:58:36 +0700

    navbar

--- src/app.tsx
+++ src/app.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import Navbar from "./components/navbar";
+import profile from './assets/png/profile.png';
 
 export default function App(): React.ReactElement {
     return (
-        <h1 className="text-3xl font-bold underline">
-            Hello world!
-        </h1>
+        <>
+            <Navbar />
+            <section id="personal" className='px-8 md:px-20 lg:px-24 py-4'>
+                <div className='flex flex-row justify-between'>
+                    <div>
+                        <div className='font-bold text-4xl'>Hi, I'm Alpin</div>
+                        <div className='font-bold text-2xl'>I'm a Software Developer</div>
+                    </div>
+                    <img src={profile} alt='profile' className='w-52'/>
+                </div>
+            </section>
+        </>
     );
 }
